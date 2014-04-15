204918e8a5c5c776cbaa8f5d893bb71b6506d48c
Lucas Affonso Xavier de Morais <lucas_axm@hotmail.com>
2014-04-15 16:42:20 -0300

    Dicionario em vetor
    Signed-off-by: Lucas Affonso Xavier de Morais <lucas_axm@hotmail.com>

--- lzw.c
+++ lzw.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "arvBin/arvBin.h"
+
+char *CriaDicionario (int);
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	char *dicionario = CriaDicionario(255);
+	printf("ASCII:\n");
+	printf("Chave\tValor\n");
+	for (int i = 0; i < 127; ++i)
+	{
+		printf("%i\t%c\n", i,dicionario[i]);
+	}
 	return 0;
+}
+
+char *CriaDicionario (int tam)
+{
+	int i;
+	char *d = (char *) malloc (sizeof(char)*tam);
+	for (int i = 0; i < 127; i++)
+		d[i]=(char) i;
+	return d;
 }
