737e8d064bd906b30933922e08c9861a82d4df81
Lucas Affonso Xavier De Morais <lucas_axm@hotmail.com>
2014-04-17 16:54:45 -0300

    percebi que sou burro. não mais usando dicionario em arquivo. compressão quase completa.

--- lzw.c
+++ lzw.c
@@ -6,12 +6,14 @@
 #define DICTAMMAX 60000
 #define STRTAMMAX 11
 
-
-char **abre_dicionario (char **, unsigned short int, FILE *);
+void compacta (char **, unsigned short int *, FILE *, FILE *);
+unsigned short int acha_nova_string (char **, unsigned short int *, FILE *, char *);
+unsigned short int busca_dicionario (char **, unsigned short int, char *);
 char *insere_no_dicionario (int);
 FILE *abre_arquivo(const char *, const char *);
 char *ngets(char *, int, FILE *);
 void LimpaBuffer ();
+char **GeraMatrizVazia (unsigned short int, unsigned short int);
 
 int main(int argc, char const *argv[])
 {
@@ -26,37 +28,44 @@ int main(int argc, char const *argv[])
 		return 0;
 	}
 	int i,t;
-	char dicionario[DICTAMMAX][STRTAMMAX];	//vetor com o dicionario
-
+	char **dicionario = GeraMatrizVazia(DICTAMMAX,STRTAMMAX);	//vetor com o dicionario
 	for (t = 0; (argv[2][t]!='.') && (argv[2][t]!='\0') ; t++);	//descobre tamanho do nome do arquivo sem extensao
 	char arq_semext[t+1]; // "nome_do_arquivo_sem_extensao"+"\0"
-	char dicname[t+5]; // "nome_do_arquivo_sem_extensao"+".dic"+"\0"
-	for (int i = 0; i < t; i++)
-	{
+	for (i = 0; i < t; i++)
 		arq_semext[i]=argv[2][i];
-		dicname[i]=argv[2][i];
-	}
 	arq_semext[i]='\0';
-	dicname[i]='\0';
-	char pontodic[]=".dic";
-	dicname = strcat(dicname,pontodic);
-
-	FILE *dicfile = abre_arquivo(dicname,"a+");
 	unsigned short int tamdic;
-	if (!fscanf(dicfile, "%hd",&tamdic))
-	{	// novo dicionario
-		tamdic = 0;
-	}
-	else
-	{	// dicionario já existe
-		fgetc (dicfile);
-		dicionario = abre_dicionario(dicionario,tamdic,dicfile);
-	}
-/*	printf("Chave\tValor\n");
-	for (int i = 0; i < tamdic; ++i)
+	switch (argv[1][1])
 	{
-		printf("%i\t", i,dicionario[i]);
-	}*/
+		case 'c':
+		{
+			FILE *infile = abre_arquivo(argv[2],"r");
+			char outname[t+5];
+			strcpy(outname,arq_semext);
+			char pontolzw[]=".lzw";
+			strcat(outname,pontolzw);
+			FILE *outfile = abre_arquivo(outname,"w");
+			compacta (dicionario,&tamdic,infile,outfile);
+			break;
+		}
+		case 'd':
+		{
+			//descompactar
+			break;
+		}
+		default:
+		{
+			fputs ("Argumento Invalido.\n",stderr);
+			return 1;	//sai do programa caso haja parametro invalido.
+			break;
+		}
+	}
+	printf("Chave\tValor\n");
+	for (i = 0; i < tamdic; i++)
+	{
+		printf("%hd\t", i+128);
+		puts(dicionario[i]);
+	}
 	return 0;
 }
 
@@ -69,26 +78,55 @@ int conta_linhas(FILE *arq)
 	return cont;
 }
 
-char **abre_dicionario (char **d, unsigned short int tam, FILE *dicfile)
+void compacta (char **dicionario, unsigned short int *tamdic, FILE *in, FILE *out)
 {
-	int i;
-	for (!feof(dicfile))
+	char str[STRTAMMAX];
+	unsigned short int code;
+	while (!feof(in))
 	{
-		if (!fscanf (dicfile, "%hd", &i)) // le indicie
-		{
-			printf("Erro ao ler tamanho do dicionario do arquivo\n");
-			exit(1);
-		}
-		fgetc(dicfile); // le separador
-
-		d[i] = ngets(d[i],10,dicfile);
+		code = acha_nova_string(dicionario, tamdic, in, str);
+		strcpy(dicionario[*tamdic],str);
+		(*tamdic)++;
+		fprintf(out, "%hd", code);
+		if (!feof(in))
+			fseek (in,-1,SEEK_CUR);
 	}
-	return d;
 }
 
-char *insere_no_dicionario (int n)
+unsigned short int acha_nova_string (char **dicionario, unsigned short int *tamdic, FILE *in, char *str)
 {
-	return d;
+	int i, achei=0;
+	unsigned short int code;
+	for (i = 0; i < STRTAMMAX; i++)
+		str[i]=0;
+	i=0;
+	while ( (!achei) && (i<11) )
+	{
+		str[i]=fgetc(in);
+		if (strlen(str)==1)
+		{
+			code=str[0];
+			i++;
+		}
+		else if (code = busca_dicionario(dicionario, *tamdic, str))
+			i++;
+		else
+		{
+			code = (*tamdic)+1;
+			(*tamdic)++;
+			achei = 1;
+		}
+	}
+	return code;
+}
+
+unsigned short int busca_dicionario (char **dicionario, unsigned short int tamdic, char *str)
+{
+	unsigned short int i;
+	for (i = 0; i < tamdic; i++)
+		if (!strcmp(dicionario[i],str))
+			return i+128;
+	return 0;
 }
 
 FILE *abre_arquivo(const char *path, const char *mode)
@@ -115,16 +153,24 @@ void LimpaBuffer ()
 /*---------------------------------------------------------------------------*/
 char *ngets (char *str, int n, FILE *fp)
 {
-	str = fgets (str, n, stdin);
+	str = fgets (str, n, fp);
 	if (str[strlen(str)-1]=='\n')
 		str[strlen(str)-1]='\0';
-	else
-		LimpaBuffer();
+/*	else
+		LimpaBuffer();*/
 	return str;
 }
 
-
-/*3
-A_
-_A
-AS*/
+char **GeraMatrizVazia (unsigned short int linhas, unsigned short int colunas)
+/* Gera matriz identidade 'linhas'x'colunas' */
+{
+	unsigned short int i,j;
+	char **matriz = (char **) malloc (sizeof(char *)*linhas);
+	for (i=0; i<linhas; i++)
+	{
+		matriz[i] = (char *) malloc (sizeof(char)*colunas);
+		for (j=0; j<colunas; j++)
+				matriz[i][j]=0;
+	}
+	return matriz;
+}
